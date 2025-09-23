8d7b2e17a59988f320316fb80b931989ba981ea8
Shamshieva Dayana <shamshievad05@gmail.com>
2025-09-23 21:26:58 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 @app.errorhandler(404)
 def not_found(err):
     return "нет такой страницы", 404
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
-               <a href="/author">author</a>
+               <a href="/lab1/author">author</a>
            </body>
         </html>""", 200, {"X-Server": "sample", 
                           "Content-Type": "text/plain; charset=utf-8"
                           }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Шамшиева Даяна Артуровна"
     group = "ФБИ-32"
@@ -29,7 +29,7 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body>
         </html>"""
 
@@ -51,7 +51,7 @@ def image ():
 
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -69,22 +69,22 @@ def counter():
         Запрошенный адрес: ''' + url + '''<br>
         Ваш IP-адресс: ''' + client_ip + '''<br>
         <hr>
-        <a href="/counter/reset">Очистка счётчика</a>
+        <a href="/lab1/counter/reset">Очистка счётчика</a>
     </body>
 </html>
 '''
 
 visit_count = 0
 
-@app.route('/counter/reset')
+@app.route('/lab1/counter/reset')
 def reset_counter():
     global count
     count = 0
-    return redirect("/counter")
+    return redirect("/lab1/counter")
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 @app.route("/lab1/created")
 def created():
     return '''
