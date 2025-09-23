b1c8bb0075d43a118484af0f843a7ebacb4289cd
Shamshieva Dayana <shamshievad05@gmail.com>
2025-09-23 19:56:34 +0700

    Картинка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 
@@ -27,3 +27,15 @@ def author():
                 <a href="/web">web</a>
             </body>
         </html>"""
+@app.route('/lab1/image')
+def image ():
+    path = url_for("static", filename="mili.jpg")
+    return '''
+<!doctype html>
+<html>
+    <body style="text-align: center; background-color: #8B0000">
+        <h1 style="color:white">Котик</h1>
+        <img src="''' + path + '''" style="-webkit-mask-image: radial-gradient(circle, black 45%, transparent 60%)">
+    </body>
+</html>
+'''
