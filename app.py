f8265b3f05100fb1d4313ce9209391ff0f8cd609
Shamshieva Dayana <shamshievad05@gmail.com>
2025-10-07 21:46:16 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -416,3 +416,6 @@ def example():
     return render_template('example.html', 
                            name=name, number=number, group=group,
                            course=course, fruits=fruits)
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
