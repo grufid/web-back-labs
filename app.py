d3f2f8e323489eaf57fc301560d07280c1c3e99b
Shamshieva Dayana <shamshievad05@gmail.com>
2025-10-07 21:08:07 +0700

    Условия в шаблоне

--- app.py
+++ app.py
@@ -405,8 +405,10 @@ def add_flower(name):
 
 @app.route('/lab2/example/')
 def example():
-    name = "Даяна Шамшиева"
-    number = "2"
-    group = "32"
-    course = "3"
-    return render_template('example.html', name=name, number=number, group=group, course=course)
+    context = {
+    'name': "Даяна Шамшиева",
+    'number':  "2",
+    'group': "32",
+    'course': "3"
+    }
+    return render_template('example.html', **context)
