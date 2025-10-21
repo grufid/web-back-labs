bdd7b9a6b874ce1fa14b24bf0acfae9655e49e7c
Shamshieva Dayana <shamshievad05@gmail.com>
2025-10-22 00:42:41 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -66,3 +66,31 @@ def pay():
 def success():
     price = request.args.get('price', 0)
     return render_template('lab3/paid.html', price=price)
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    text_transform = request.args.get('text_transform')
+
+    if any([color, bg_color, font_size, text_transform]):
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if bg_color:
+            resp.set_cookie('bg_color', bg_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        if text_transform:
+            resp.set_cookie('text_transform', text_transform)
+        return resp
+    
+    color = request.cookies.get('color', '#000000')
+    bg_color = request.cookies.get('bg_color', '#ffffff')
+    font_size = request.cookies.get('font_size', '16')
+    text_transform = request.cookies.get('text_transform', 'none')
+    
+    resp = make_response(render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size, text_transform=text_transform))
+    return resp
+
