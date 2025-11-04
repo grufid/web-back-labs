94914cdc36d151c262ec2e410ea862ec97f760ba
Shamshieva Dayana <shamshievad05@gmail.com>
2025-11-04 21:28:09 +0700

    Валидация

--- lab4.py
+++ lab4.py
@@ -111,8 +111,10 @@ def tree():
     operation = request.form.get('operation')
 
     if operation == 'cut':
-        tree_count -=1
+        if tree_count > 0:
+            tree_count -=1
     elif operation == 'plant':
-        tree_count += 1
+        if tree_count < 10:
+            tree_count += 1
 
     return redirect('/lab4/tree')
