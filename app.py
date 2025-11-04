486d6a28a3f008e22720f73150f9a8b1745322f9
Shamshieva Dayana <shamshievad05@gmail.com>
2025-11-04 19:21:09 +0700

    Заготовка к 4-ой лабораторной работе

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 import datetime
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
-
+app.register_blueprint(lab4)
 
 visit_log = []
 
@@ -212,6 +213,9 @@ def index():
             </li>
             <li>
                 <a href="/lab3">Лабораторная III</a>
+            </li>
+            <li>
+                <a href="/lab4">Лабораторная IV</a>
             </li>
         </main>
         <footer>
