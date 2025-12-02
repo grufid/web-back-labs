743d3941a91853560e6f0955daf891ee185b5d87
Shamshieva Dayana <shamshievad05@gmail.com>
2025-12-02 22:27:09 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -50,7 +50,35 @@ def api():
                     'result': 'success',
                     'id': id
                 }
-
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if office['tenant'] == '':
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office is not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'You are not the tenant of this office'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ''
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+            
     return {
             'jsonrpc': '2.0',
             'error': {
@@ -59,4 +87,6 @@ def api():
             },
             'id': id
         }
+
+
 
