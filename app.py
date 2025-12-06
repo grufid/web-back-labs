4124afec51bb4050268632556afecab9ea1ebef7
Shamshieva Dayana <shamshievad05@gmail.com>
2025-12-06 23:14:00 +0700

    Добавление..

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab6 import lab7
 
 import datetime
 import os
@@ -20,6 +21,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 visit_log = []
 
